d4738f6dceb422e97579b689eb8a917231225934
Diana <diana.sinenchenko@gmail.com>
2018-01-19 16:16:48 +0200

    add action button1_Click

--- spelling/Form1.cs
+++ spelling/Form1.cs
@@ -19,7 +19,37 @@ namespace spelling
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var Ворд1 = new Microsoft.Office.Interop.Word.Application();
+            Ворд1.Visible = false;
+            // Открываем новый документ MS Word:
+            Ворд1.Documents.Add();
+            // Копируем содержимое текстового окна в документ
+            Ворд1.Selection.Text = textBox1.Text;
+            // Непосредственная проверка орфографии:
+            Ворд1.ActiveDocument.CheckSpelling();
+            // Копируем результат назад в текстовое поле
+            textBox1.Text = Ворд1.Selection.Text;
+            Ворд1.Documents.Close(false);
+            // Закрыть документ Word без сохранения:
+            Ворд1.Quit();
+            Ворд1 = null;
+
+
+
+
+
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear(); button1.Text = "Проверка орфографии";
+
+
+
+        }
+
+
+
     }
 }
